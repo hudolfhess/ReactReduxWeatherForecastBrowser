0c23b5cf5a9625a331742a7d873b8773ec0c6427
Hudolf Hess <hudolf@gmail.com>
2017-03-08 16:06:15 -0300

    Started SearchBar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import SearchBar from '../containers/search_bar'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Ola
+        <SearchBar />
       </div>
     )
   }
